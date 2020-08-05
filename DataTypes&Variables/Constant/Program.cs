e8198799f1e0c7b31926f0dd0edddca9baf2db90
phongctnguyen <phongctnguyen@gmail.com>
2020-08-05 23:49:25 +0700

    finish DataType and Variables Section

---
+++ DataTypes&Variables/Constant/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Constant
+{
+    class Program
+    {
+        // constant as fields
+        const double PI = 3.14;
+        const int weeks = 52, months = 12;
+        const string birthDay = "28/10/1994";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
