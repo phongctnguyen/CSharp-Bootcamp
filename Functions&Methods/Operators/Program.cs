bc2058f8e08f5c4c700ffba81c23d2381ccba0ac
phongctnguyen <phongctnguyen@gmail.com>
2020-08-09 01:46:04 +0700

    finish functions and methods section

---
+++ Functions&Methods/Operators/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 3;
+            int num3;
+
+            // unary operators
+            num3 = - num1;
+            Console.WriteLine($"num3 is {num3}");
+
+            bool isSunny = true;
+            Console.WriteLine($"Is it sunny {!isSunny}");
+
+            // increment operator
+            int num = 0;
+            num++;
+            Console.WriteLine($"num is {num}");
+
+            // decrement operator
+            num--;
+            Console.WriteLine($"num is {num}");
+
+            // + - * /
+
+            // relational and type operators (<, >, ==, !=)
+            bool isLower;
+            isLower = num1 < num2;
+            Console.WriteLine(isLower);
+
+            // conditional operators (&&,, ||)
+        }
+    }
+}
